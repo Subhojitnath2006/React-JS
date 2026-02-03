d014de09bb838adcc3d6bf020efa2eb2c8206246
Subhojit <rabinath750@gmail.com>
2026-02-03 07:17:56 +0530

    basic react

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const areactElement = React.createElement(
+  'a',
+  {href: 'https://www.google.com',target:"_blank"},
+  'click to visit google'
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    //areactElement
+     <App/>
 )
